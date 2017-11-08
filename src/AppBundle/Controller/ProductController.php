2909160cfae615f79dae3a78d177ffa59e83e442
jonathan-klh <jonathan@klh-competence.com>
2017-11-08 23:47:54 +0100

    crud product

---
+++ src/AppBundle/Controller/ProductController.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Product;
+use AppBundle\Entity\Shop;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Product controller.
+ *
+ * @Route("product")
+ */
+class ProductController extends Controller
+{
+
+    /**
+     * Creates a new product entity.
+     *
+     * @Route("/new/{id}", name="product_new")
+     * @Method({"GET", "POST"})
+     */
+    public function newAction(Request $request, Shop $shop)
+    {
+        $product = new Product();
+        $form = $this->createForm('AppBundle\Form\ProductType', $product);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $product->setShop($shop);
+            $em->persist($product);
+            $em->flush();
+
+            return $this->redirectToRoute('shop_show', array('id' => $shop->getId()));
+        }
+
+        return $this->render('product/new.html.twig', array(
+            'product' => $product,
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * Finds and displays a product entity.
+     *
+     * @Route("/{id}", name="product_show")
+     * @Method("GET")
+     */
+    public function showAction(Product $product)
+    {
+        $deleteForm = $this->createDeleteForm($product);
+
+        return $this->render('product/show.html.twig', array(
+            'product' => $product,
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing product entity.
+     *
+     * @Route("/{id}/edit", name="product_edit")
+     * @Method({"GET", "POST"})
+     */
+    public function editAction(Request $request, Product $product)
+    {
+        $deleteForm = $this->createDeleteForm($product);
+        $editForm = $this->createForm('AppBundle\Form\ProductType', $product);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('shop_show', array('id' => $product->getShop()->getId()));
+        }
+
+        return $this->render('product/edit.html.twig', array(
+            'product' => $product,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a product entity.
+     *
+     * @Route("/delete/{id}", name="product_delete")
+     */
+    public function deleteAction(Request $request, Product $product)
+    {
+
+         $em = $this->getDoctrine()->getManager();
+            $em->remove($product);
+            $em->flush();
+
+
+        return $this->redirectToRoute('shop_show', array('id' => $product->getShop()->getId()));
+    }
+
+    /**
+     * Creates a form to delete a product entity.
+     *
+     * @param Product $product The product entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm(Product $product)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('product_delete', array('id' => $product->getId())))
+            ->setMethod('DELETE')
+            ->getForm()
+        ;
+    }
+}
