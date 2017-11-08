e97e3bd9df9ca2ba792235aa188a6d24641fa812
jonathan-klh <jonathan@klh-competence.com>
2017-11-08 22:11:02 +0100

    relations entre user/shop et shop/product

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -3,6 +3,7 @@
 
 namespace AppBundle\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
 use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
 
@@ -19,8 +20,50 @@ class User extends BaseUser
      */
     protected $id;
 
+    /**
+     * @ORM\OneToMany(targetEntity="Shop", mappedBy="user")
+     */
+    private $shops;
+
+
     public function __construct()
     {
         parent::__construct();
+        $this->shops = new ArrayCollection();
+
+    }
+
+    /**
+     * Add shop
+     *
+     * @param \AppBundle\Entity\Shop $shop
+     *
+     * @return User
+     */
+    public function addShop(\AppBundle\Entity\Shop $shop)
+    {
+        $this->shops[] = $shop;
+
+        return $this;
+    }
+
+    /**
+     * Remove shop
+     *
+     * @param \AppBundle\Entity\Shop $shop
+     */
+    public function removeShop(\AppBundle\Entity\Shop $shop)
+    {
+        $this->shops->removeElement($shop);
+    }
+
+    /**
+     * Get shops
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getShops()
+    {
+        return $this->shops;
     }
 }
